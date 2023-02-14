0d99e7880e1fc30f54867a69c20e9085357aaaf4
Louie <louiec@callboxinc.com>
2023-02-14 15:50:14 +0800

    making some ui

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat System</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div class="chat-container">
+      <div class="chat-header">
+        <h1>Chat Room</h1>
+      </div>
+      <div class="chat-messages">
+        <ul>
+          <li class="message">User 1: Hello!</li>
+          <li class="message">User 2: Hi there!</li>
+          <li class="message">User 1: How are you?</li>
+          <li class="message">User 2: I'm doing well, thanks!</li>
+        </ul>
+      </div>
+      <div class="chat-input">
+        <form>
+          <input type="text" placeholder="Type your message here">
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+</body>
+</html>
