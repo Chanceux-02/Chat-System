523c591a04c68f83b321be3a347843d85ab5e4bc
Louie <louiec@callboxinc.com>
2023-02-15 10:01:43 +0800

    making the scoll from the bottom in jquery

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function() { // making the script ready after the structure load
     // Set the interval for fetching the data in milliseconds
-    var interval = 50; // 5 seconds
+    var interval = 50; // 50 miliseconds
 
     // Define the function that fetches the data
     function fetchData() {
         $.ajax({
-            url: '../fetch.php',
-            type: 'GET',
-            success: function(data) {
+            url: '../fetch.php', //the file url
+            type: 'GET', // the method
+            success: function(data) { //if ajax is success then
                 // Update the HTML element with the fetched data
-                $('#result').html(data);
-                $('#result').scrollTop($('#result')[0].scrollHeight);
+                $('#result').html(data); //targeting the element where to update the data
+                $('#result').scrollTop($('#result')[0].scrollHeight); //to make the scroll from the bottom or the latest of the chat
             },
-            error: function(jqXHR, textStatus, errorThrown) {
+            error: function(jqXHR, textStatus, errorThrown) { // if ajax is not success
                 console.log(textStatus, errorThrown);
             }
         });
